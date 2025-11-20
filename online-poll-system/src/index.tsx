428e6ec38733fa135af16d0acbdc0b5ee1e23ebe
letisiaelias-beep <letisiaelias@gmail.com>
2025-11-20 17:00:49 +0300

    wraping src/index.tsx

--- online-poll-system/src/index.tsx
+++ online-poll-system/src/index.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
