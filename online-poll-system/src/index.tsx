286b4e3568f855c577f8edfdc700d888bd7aee54
letisiaelias-beep <letisiaelias@gmail.com>
2025-11-25 19:00:49 +0300

    fixing styles and css

--- online-poll-system/src/index.tsx
+++ online-poll-system/src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import "./index.css";
 import App from "./App";
-import { store } from "./store/store"; // adjust path if your store file is elsewhere
-import "./index.css"; // or App.css if you prefer
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
